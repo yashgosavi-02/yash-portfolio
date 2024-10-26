a2e87e72af97517a7f2bf3cdaf4e86ca99a8235a
Yash Gosavi <iyashgosavi02@gmail.com>
2024-10-26 10:12:32 +0530

    add primary background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,8 +22,12 @@ module.exports = {
 	},
   	extend: {
   		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
+  			primary : '#1c1c22',
+			accent : {
+				DEFAULT: '#00ff99',
+				hover : '#00e187',
+			}
+  			
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
